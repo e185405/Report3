110ede0dd0ed197dbd309710701b8d8ae6a49cb6
Kosuke Asaka <e185405@ie.u-ryukyu.ac.jp>
2019-11-22 17:49:23 +0900

    LivingThingを継承してEnemy,Heroクラスを修正した。

---
+++ src/jp/ac/uryukyu/ie/e185405/LivingThing.java
@@ -0,0 +1,45 @@
+package jp.ac.uryukyu.ie.e185405;
+
+public class LivingThing {
+    String name;
+    int hitPoint;
+    int attack;
+    boolean dead;
+
+    public LivingThing(String name,int maxHP,int attack){
+        this.name = name;
+        hitPoint = maxHP;
+        this.attack = attack;
+        dead = false;
+        System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maxHP, attack);
+    }
+
+    public boolean isDead() {
+        return dead;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public void attack(LivingThing livingThing) {
+        int damage = (int) (Math.random() * attack);
+        if (dead) {
+            damage = 0;
+        } else {
+            System.out.printf("%sの攻撃！%sに%dのダメージを与えた！！\n", name, livingThing.getName(), damage);
+            livingThing.wounded(damage);
+        }
+    }
+    public void wounded(int damage){
+        hitPoint -= damage;
+        if( hitPoint < 0 ) {
+            dead = true;
+            System.out.printf("%sは倒れた。\n", name);
+        }
+    }
+
+
+
+
+    }
